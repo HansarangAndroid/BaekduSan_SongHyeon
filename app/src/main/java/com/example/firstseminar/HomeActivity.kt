e0f39cdbcd2e583fc69dea751dc0c82cda23a386
ssonghyun101 <songhyun0868@gmail.com>
2021-06-21 19:39:48 +0900

    로그아웃 구현 성공

--- app/src/main/java/com/example/firstseminar/HomeActivity.kt
+++ app/src/main/java/com/example/firstseminar/HomeActivity.kt
@@ -1,16 +1,36 @@
 package com.example.firstseminar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.firstseminar.SharedPrference.CherishUserAuthStorage
+import com.example.firstseminar.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        Toast.makeText(this,"로그인 성공", Toast.LENGTH_SHORT).show()
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        OnClickLogout()
+
+
     }
+    private fun OnClickLogout(){
+        binding.logoutBtn.setOnClickListener{
+            CherishUserAuthStorage.clearAuthStorage(this)
+            intent = Intent(this@HomeActivity,SignInActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+
 
     override fun onStart() {
         super.onStart()
@@ -41,3 +61,4 @@ class HomeActivity : AppCompatActivity() {
         Log.d("자기소개화면","onDestroy")
     }
 }
+
